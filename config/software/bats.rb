b25ca98a37ab89b99b5341197cf9abb1f4eefc70
Tom Duffield <tom@opscode.com>
2014-09-30 13:39:02 -0500

    Add bats software
    Specifically, for use in chef-container

---
+++ config/software/bats.rb
@@ -0,0 +1,28 @@
+#
+# Copyright:: Copyright (c) 2014 Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "bats"
+default_version "0.4.0"
+
+source git: "https://github.com/sstephenson/bats.git"
+
+relative_path "bats"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  command "#{project_dir}/install.sh #{install_dir}/embedded", env: env
+end
